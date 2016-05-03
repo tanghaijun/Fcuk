a688457a2ddff3d019cda9d63ce747a532c222b2
tanghaijun <454609606@qq.com>
2016-05-03 10:59:18 +0800

    aaa

---
+++ FuckApi/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace FuckApi.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<string> Index()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+    }
+}
---
+++ FuckApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FuckApi.WebApiApplication" Language="C#" %>
