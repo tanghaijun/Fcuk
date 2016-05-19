955d0fd30cf6393fe25d747cf6b6c870ac721819
y8932809 <529030008@qq.com>
2016-05-19 09:02:19 +0800

    node连接api项目读取数据

---
+++ FuckApi/Controllers/UserInfoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FuckApi.Models;
+namespace FuckApi.Controllers
+{
+    public class UserInfoController : ApiController
+    {
+        // GET api/userinfo
+        public HttpResponseMessage Get()
+        {
+
+            Test test = new Test();
+            test.Id = "1";
+            test.Name = "yukaifei";
+            var result = Common.Convert.toJson(test);
+            return result;
+        }
+
+        // GET api/userinfo/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/userinfo
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/userinfo/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/userinfo/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
